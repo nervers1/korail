d0763a61b67e6aaaadda29293c6c9f5cc276cfc5
Formulate <nervers1@gmail.com>
2025-02-04 17:12:02 +0900

    jUnit 설정 변경 프로퍼티 파일을 읽어서 Map으로 저장 프로퍼티 관련 확장 유틸리티 메서드 추가 프로퍼티 파일 길이부 파싱을 위해 variable 부분을 0으로 수정 jUnit 테스트 코드 작성

--- src/main/java/or/kr/formulate/korail/util/PropertyUtil.java
+++ src/main/java/or/kr/formulate/korail/util/PropertyUtil.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -13,91 +12,99 @@ public class PropertyUtil {
 
     private static final Logger logger = LoggerFactory.getLogger(PropertyUtil.class);
 
-    public static void getMetaProp(String propName) {
-        Map<String, List<Map<Integer, Object>>> map = new ConcurrentHashMap<>();
-        Map<String, Map<?,?>> result = new ConcurrentHashMap<>();
+    public static List<Map<String, Object>> getFieldList(Map<String, Object> info, String key) {
+        return (List<Map<String, Object>>) ((Map<String, Object>) info.get(key)).get("fields");
+    }
+
+    public static Map<String, Object> getMetaProp(String propName) {
         String path = "meta/" + propName + ".properties";
         String propPath = Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource(path)).getPath();
-        logger.debug("propPath ---> {}", propPath);
+        logger.debug("프로퍼티 경로 ---> {}", propPath);
 
         final Properties prop = new Properties();
-        Map<String, Object> ifMap = new TreeMap<>((o1, o2) -> o1.toString().compareTo(o2.toString()));
-        Map<Integer, Map<String, Object>> fields;
+
+        // 프로퍼티 파일을 읽는다
         try (FileInputStream stream = new FileInputStream(propPath)) {
             prop.load(stream);
-            Enumeration<Object> keys = prop.keys();
-
-            logger.debug("============================================================================");
-            while (keys.hasMoreElements()) {
-                String key = (String) keys.nextElement();
-                String[] splitKeys = key.split("\\.");
-
-                String interfaceId = splitKeys[0];
-                String propType = splitKeys[1];
-                String desc;
-                String fieldName;
-                if ("name".equalsIgnoreCase(propType)) {
-                    // IF0310.name=[0310]
-                    desc = prop.getProperty(key);
-
-                } else if ("field".equalsIgnoreCase(propType)) {
-                    // IF0310.field.name1=1|TCP/IP 송신 바이트수|N|4|4
-                    fields = new TreeMap<>();
-                    Map<String, Object> itemMap = new ConcurrentHashMap<>();
-
-                    fieldName = splitKeys[2];  // name1
-                    String[] fieldArr = prop.getProperty(key).split("[|]");
-
-                    // 1|TCP/IP 송신 바이트수|N|4|4
-                    // 2|업무구분코드|S|3|7
-                    // ...
-                    logger.debug("interfaceId: {}",interfaceId);
-                    logger.debug("name: {}",splitKeys[2]);
-                    logger.debug("idx: {}",fieldArr[0]);
-                    logger.debug("type: {}",fieldArr[2]);
-                    logger.debug("nameKr: {}",fieldArr[1]);
-                    logger.debug("length: {}",fieldArr[3]);
-                    logger.debug("offset: {}",fieldArr[4]);
-
-                    logger.debug("----------------------------------------------------------------------------");
-                    itemMap.put("interfaceId",interfaceId);
-                    itemMap.put("name",splitKeys[2]);
-                    itemMap.put("idx",Integer.parseInt(fieldArr[0]));
-                    itemMap.put("type",fieldArr[2]);
-                    itemMap.put("nameKr",fieldArr[1]);
-                    itemMap.put("length",Integer.parseInt(fieldArr[3]));
-                    itemMap.put("offset",Integer.parseInt(fieldArr[4]));
-                    fields.put(Integer.parseInt(fieldArr[0]), itemMap);
-
-//
-//                    fields.add(fieldMap);
-                }
-//                result.put("id", interfaceId);
-
-            }
-            logger.debug("----------------------------------------------------------------------------");
-            logger.debug("============================================================================");
-            ifMap.forEach((k, v) -> {
-                logger.debug("ID: {} --> : {}", k, v);
-
-            });
-            logger.debug("============================================================================");
-//            fields.forEach(field -> {
-//                logger.debug(field.toString());
-//            });
-            logger.debug("============================================================================");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+        // 키값의 Enumeration 객체
+        Enumeration<Object> keys = prop.keys();
+        // 인터페이스 맵 선언
+        Map<String, Object> ifMap = new ConcurrentHashMap<>();
+        // 설정파일에 기술된 전체 필드 리스트
+        List<Map<String, Object>> fields = new ArrayList<>();
+
+        // IF0300.name=[0300]
+        while (keys.hasMoreElements()) {
+            String key = (String) keys.nextElement();
+            String[] splitKeys = key.split("\\."); // key String 값을 '.'을 기준으로 분리
+
+            String interfaceId = splitKeys[0];
+            String propType = splitKeys[1];
+            Map<String, Object> fieldItem = new TreeMap<>();
+
+            if ("name".equalsIgnoreCase(propType)) {
+                Map<String, Object> info = new ConcurrentHashMap<>();
+                info.put("interfaceId", interfaceId);
+                info.put("desc", prop.getProperty(key));
+                ifMap.put(interfaceId, info);
+            } else if ("field".equalsIgnoreCase(propType)) {
+
+                // IF0310.field.name1=1|TCP/IP 송신 바이트수|N|4|4
+                Map<String, Object> itemMap = new ConcurrentHashMap<>();
+                String[] fieldArr = prop.getProperty(key).split("[|]");
+
+                // 1|TCP/IP 송신 바이트수|N|4|4
+                // 2|업무구분코드|S|3|7
+                // ...
+
+                String name = splitKeys[2];
+                int idx = Integer.parseInt(fieldArr[0]);
+                String type = fieldArr[2];
+                String nameKr = fieldArr[1];
+                int length = Integer.parseInt(fieldArr[3]);
+                int offset = Integer.parseInt(fieldArr[4]);
+
+                itemMap.put("interfaceId", interfaceId);
+                itemMap.put("name", name);
+                itemMap.put("idx", idx);
+                itemMap.put("type", type);
+                itemMap.put("nameKr", nameKr);
+                itemMap.put("length", length);
+                itemMap.put("offset", offset);
+
+                fieldItem.put(interfaceId, itemMap);
+                fields.add(fieldItem);
+
+            }
+
+        }
+
+        // 전문 필드 정보를 인터페이스 맵에 저장
+        ifMap.forEach((ifKey, ifValue) -> {
+
+            List<Map<String, Object>> temp = new ArrayList<>();
+            Map<String, Object> ifItem = (Map<String, Object>) ifValue;
+
+            fields.forEach(field -> {
+                field.keySet().forEach(key -> {
+                    if (ifKey.equalsIgnoreCase(key)) {
+                        temp.add((Map<String, Object>) field.get(key));
+                    }
+                });
+            });
+
+            // "idx' 필드값 기준으로 오름차순 정렬
+            temp.sort(Comparator.comparingInt(o -> (Integer) o.get("idx")));
+
+            ifItem.put("fields", temp);
+        });
+
+        return ifMap;
     }
 
 
-    public static void main(String[] args) {
-        String path = "test";
-        getMetaProp(path);
-
-    }
 }
---
+++ src/test/java/or/kr/formulate/korail/util/PropertyUtilTest.java
@@ -0,0 +1,45 @@
+package or.kr.formulate.korail.util;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+
+import static or.kr.formulate.korail.util.PropertyUtil.getFieldList;
+import static or.kr.formulate.korail.util.PropertyUtil.getMetaProp;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class PropertyUtilTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(PropertyUtilTest.class);
+
+    @Test
+    public void PropertyUtilTest() {
+
+        // 메타정보위치에서 프로퍼티 파일을 읽어온다 ( test.properties 파일을 읽는 경우 파일명 "test"를 입력한다.
+        String path = "test";
+        Map<String, Object> info = getMetaProp(path);
+        logger.debug("info ---> {}", info);
+
+        // 전문정보를 추출한다.
+        logger.debug("============================[전문정보추출]================================================");
+        info.forEach((k, v) -> {
+            logger.debug("ID: {} --> : {}", k, v);
+
+        });
+
+        // 메타정보를 맵 형태로 읽어온 경우 해당 필드 목록을 맵에서 추출한다.
+        List<Map<String, Object>> if0310 = getFieldList(info, "IF0310");
+
+        // 추출된 맵의 리스트를 출력한다.
+        if0310.forEach(field -> {
+            field.forEach((k, v) -> {
+                logger.debug("ID: {} --> : {}", k, v);
+            });
+        });
+
+    }
+}

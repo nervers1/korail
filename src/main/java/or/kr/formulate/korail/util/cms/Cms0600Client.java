6151127340dc3373f5b4c3741c3e1f933e269b04
Formulate <nervers1@gmail.com>
2025-02-20 17:45:42 +0900

    Blocking TCP로 전환 : 외부 연계에서 정상 수신을 위해

--- src/main/java/or/kr/formulate/korail/util/cms/Cms0600Client.java
+++ src/main/java/or/kr/formulate/korail/util/cms/Cms0600Client.java
@@ -3,9 +3,10 @@ package or.kr.formulate.korail.util.cms;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
@@ -19,85 +20,31 @@ import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * CMS Client : CMS 파일 전송을 위해 파일을 읽어 CMS 서버로 송신한다.
+ * - 이때 보내지는 파일은 블럭과 시퀀스로 나누어 전송한다.
+ * - 블럭의 갯수는 0001 ~ 9999개 이내, 한 블럭은 1 ~ 100 SEQUENCE로 구성
+ * - 시퀀스는 데이터의 1 Row를 기준으로 함(한 블럭은 100건의 데이터, 1 ~ 100개의 SEQUENCE로 구성)
+ */
 public class Cms0600Client {
     private static final Logger logger = LoggerFactory.getLogger(Cms0600Client.class);
 
-
-    ExecutorService executorService;
-    final int capacity = 1024;
-    final int port = 8000;
-    final int clientPort = 8080;
-
-
-    private void startClient() throws Exception {
-        SocketChannel socketChannel = SocketChannel.open();
-        socketChannel.connect(new InetSocketAddress("localhost", port));
-        socketChannel.configureBlocking(true);
-        System.out.println("Socket connection established");
-
-        final Selector selector = Selector.open();
-        socketChannel.register(selector, SelectionKey.OP_READ);
-
-        new Thread(() -> {
-            while (true) {
-                try {
-                    selector.select();
-                    Set<SelectionKey> selectionKeys = selector.selectedKeys();
-                    for (Iterator<SelectionKey> iterator = selectionKeys.iterator(); iterator.hasNext(); ) {
-                        SelectionKey key = iterator.next();
-
-                        if (key.isReadable()) {
-                            SocketChannel sc = (SocketChannel) key.channel();
-                            ByteBuffer byteBuffer = ByteBuffer.allocate(capacity);
-                            int bytesRead = sc.read(byteBuffer);
-
-                            if (bytesRead == -1) {
-                                System.out.println("TCP connection closed");
-                                key.cancel();
-                            } else {
-                                byteBuffer.flip();
-                                byte[] content = new byte[bytesRead];
-                                byteBuffer.get(content, 0, bytesRead);
-
-                                System.out.println("Received: \n[" + new String(content, StandardCharsets.UTF_8) + "]");
-                            }
-                        }
-                    }
-                } catch (Exception e) {
-                    System.out.println("Error: " + e);
-                }
-            }
-        }, "selector thread").start();
-
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            String line = scanner.nextLine();
-
-            if (Objects.equals(line, "exit")) break;
-
-            ByteBuffer buffer = ByteBuffer.allocate(capacity);
-            buffer.clear();
-            buffer.put(line.getBytes(StandardCharsets.UTF_8));
-            buffer.flip();
-
-            while (buffer.hasRemaining()) {
-                socketChannel.write(buffer);
-            }
-        }
-
-        socketChannel.close();
-        System.out.println("TCP connection closed");
-    }
-
     public static void main(String[] args) {
-        Cms0600Client ex = new Cms0600Client();
-        try {
-            ex.startClient();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        } finally {
-            //
-        }
 
+
+        try (CmsSocket socket = new CmsSocket("localhost", 8080);
+             OutputStream os = socket.getOutputStream();
+             DataOutputStream dos = new DataOutputStream(os);
+             InputStream is = socket.getInputStream();
+             DataInputStream dis = new DataInputStream(is)) {
+
+            logger.debug("Connected {}, From {}", socket.getLocalPort(), socket.getRemoteSocketAddress().toString());
+            socket.connect(new InetSocketAddress("localhost", 8080));
+
+            BufferedInputStream bis = new BufferedInputStream(is);
+
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
     }
 }
